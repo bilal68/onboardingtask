7b04cd562c0949234f69320ca7d19450458f3492
Naeem <naeemark@gmail.com>
2019-12-21 17:51:58 +0800

    Issue: #13 Frequency Service

--- src/services/frequency/frequency.spec.js
+++ src/services/frequency/frequency.spec.js
@@ -3,7 +3,7 @@ const httpStatus = require('http-status');
 const service = require('./frequency.service');
 
 
-describe('Service - rate', () => {
+describe('Service - frequency', () => {
 
   beforeEach(() => { });
 
@@ -12,9 +12,28 @@ describe('Service - rate', () => {
   });
 
   it('should return the frequency', () => {
+    process.env.DURATION_CRON_INDEX = '0';
     return service.getFrequency().then((response) => {
       expect(response).toBeObject();
       expect(response).toContainKey('minutes');
     });
   });
+
+  it('should return the frequency without evn', () => {
+    return service.getFrequency().then((response) => {
+      expect(response).toBeObject();
+      expect(response).toContainKey('minutes');
+    });
+  });
+
+
+  it('should set the frequency', () => {
+    return service.setFrequency(15).then((response) => {
+      expect(response).toBeObject();
+      expect(response).toContainKey('minutes');
+    });
+  });
+
+
+
 });
