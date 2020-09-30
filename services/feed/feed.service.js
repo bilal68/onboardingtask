f1e7f2fbbadf58e73316fb6cf18e4ac5a713d454
FIVERIVERSTECH\bilal.amin <bilal.amin@fiverrivertech.com>
2020-09-30 16:23:27 +0500

    jest.js done

--- services/feed/feed.service.js
+++ services/feed/feed.service.js
@@ -2,37 +2,41 @@ const moment = require("moment")
 
 const fs = require("fs")
 const parse = require("csv-parse")
-const staticBasePath = "./bitcoin1.csv"
+const staticBasePath = "./bitcoin.csv"
 
-const getData = async (rangeStart, rangeEnd) => {
-    console.log('in get data')
+const getData = async (rangeStart, rangeEnd, filePath = staticBasePath) => {
   try {
-    const csvData = []
     const start = moment(rangeStart).unix()
     const end = moment(rangeEnd).unix()
 
-    return new Promise((resolve, reject) => {
-      fs.createReadStream(staticBasePath)
-        .pipe(parse({ delimiter: "," }))
-        .on("data", function (csvrow) {
-          // console.log(csvrow[0] > start && csvrow[0] <= end)
-          if (csvrow[0] > start && csvrow[0] <= end) {
-            csvData.push(csvrow)
-          }
-        })
-        .on("end", function () {
-          resolve({
-            response: {
-              count: csvData.length,
-              firstObject: csvData[0],
-              lastObject: csvData[csvData.length - 1],
-            },
-          })
-        })
-    })
+    const result = await readFile(start, end, filePath)
+    if (result) {
+      return (response = {
+        count: result.length,
+        firstObject: result[0],
+        lastObject: result[result.length - 1],
+      })
+    }
   } catch (error) {
-    reject(new Error(error))
+    throw new Error(error)
   }
+}
+
+const readFile = async (start, end, filePath) => {
+  const csvData = []
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(filePath)
+      .pipe(parse({ delimiter: "," }))
+      .on("data", function (csvrow) {
+        if (csvrow[0] > start && csvrow[0] <= end) {
+          csvData.push(csvrow)
+        }
+      })
+      .on("end", function () {
+        resolve(csvData)
+      })
+      .on("error", (error) => reject(error))
+  })
 }
 
 module.exports = {
---
+++ services/feed/feed.spec.js
@@ -0,0 +1,12 @@
+const service = require("./feed.service")
+
+describe("GET DATA FUNC TEST", () => {
+  it("it should return some response object", async () => {
+    const result = await service.getData(
+      "2016-06-15T00:00:00",
+      "2017-07-15T23:59:59"
+    )
+    expect(typeof result).toBe("object")
+    expect(result.response).toHaveProperty("count")
+  })
+})
