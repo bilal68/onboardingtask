7875bf04ea50229b418cbed4358b6c4fdf203c3e
FIVERIVERSTECH\bilal.amin <bilal.amin@fiverrivertech.com>
2020-09-30 16:23:48 +0500

    tweaks in test

--- services/feed/feed.spec.js
+++ services/feed/feed.spec.js
@@ -4,7 +4,8 @@ describe("GET DATA FUNC TEST", () => {
   it("it should return some response object", async () => {
     const result = await service.getData(
       "2016-06-15T00:00:00",
-      "2017-07-15T23:59:59"
+      "2017-07-15T23:59:59",
+      './bitcoin1.csv'
     )
     expect(typeof result).toBe("object")
     expect(result.response).toHaveProperty("count")
