c9a3ffae9720d5e246dcae1cce371ec8e01ddac0
FIVERIVERSTECH\bilal.amin <bilal.amin@fiverrivertech.com>
2020-10-02 18:07:39 +0500

    tt

--- routes/feed/feed.controller.js
+++ routes/feed/feed.controller.js
@@ -1,18 +1,6 @@
 const { getData } = require("../../services/feed")
 
 exports.get = async (req, res, next) => {
-  // try {
-    const response = await getData(req.body.rangeStart, req.body.rangeEnd)
-    return res.json(response)
-  // } catch (error) {
-  //   res.json({
-  //     responseCode: 404,
-  //     responseMessage: "Failure",
-  //     response: {
-  //       error: {
-  //         message: error.message,
-  //       },
-  //     },
-  //   })
-  // }
+  const response = await getData(req.body.rangeStart, req.body.rangeEnd)
+  return res.json(response)
 }
